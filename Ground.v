19acb9b067fac711f0a88fc6c411ce9c3b458cea
HaoGuangBo <haoguangbo@yeah.net>
2017-01-01 11:10:47 +0800

    ground test

--- Ground.v
+++ Ground.v
@@ -58,29 +58,34 @@ module Ground (
         // end
         if (game_status) begin
             if (row_addr>=10'd400 && row_addr<10'd408) begin
-                px <= 1'b1;
+                px<=pattern[(col_addr%10'd40+ground_position+(row_addr-10'd400)*40)%10'd320];
+                //px <= 1'b1;
             end else begin
                 px <= 1'b0;
             end
-            if(ground_position<3'd40)begin
-                ground_position<=ground_position+speed;
-                // px[319:1]=pattern[318:0];
-                // px[0]=pattern[319];
-            end else begin
-                ground_position<=6'b0;
-                speed<=1'd4;
-                // px[316:4]=pattern[315:0];
-                // px[3:0]=pattern[319:316];
-                pattern[39:0]<=40'b0000000000000000000111100000000000000000;
-                pattern[79:40]<=40'b0000000000000000011000011100000000000000;
-                pattern[119:80]<=40'b1111111111111111100000000011111111111111;
-                pattern[159:120]<=40'b0000000000000000000000110000000000000000;
-                pattern[199:160]<=40'b0000000111000000000000000000000000000000;
-                pattern[239:200]<=40'b1000000000000000000000000000000000111111;
-                pattern[279:240]<=40'b0000000000000000000000011110000000000000;
-                pattern[319:280]<=40'b0000000000000000000000000000000000000000;
-            end
+            ground_position<=(ground_position+speed)%3'd40;
+            //NO NEED:
+            // if(ground_position+speed<3'd40)begin
+            //     ground_position<=ground_position+speed;
+            //     // px[319:1]=pattern[318:0];
+            //     // px[0]=pattern[319];
+            // end else begin
+            //     ground_position<=(ground_position+speed)%3'd40;
+            // end
             // pattern[319:0]=px[319:0];
+        end else begin
+            ground_position<=6'b0;
+            speed<=1'd4;
+            // px[316:4]=pattern[315:0];
+            // px[3:0]=pattern[319:316];
+            pattern[39:0]<=40'b0000000000000000000111100000000000000000;
+            pattern[79:40]<=40'b0000000000000000011000011100000000000000;
+            pattern[119:80]<=40'b1111111111111111100000000011111111111111;
+            pattern[159:120]<=40'b0000000000000000000000110000000000000000;
+            pattern[199:160]<=40'b0000000111000000000000000000000000000000;
+            pattern[239:200]<=40'b1000000000000000000000000000000000111111;
+            pattern[279:240]<=40'b0000000000000000000000011110000000000000;
+            pattern[319:280]<=40'b0000000000000000000000000000000000000000;
         end
     end
     
